0fffa3a4383d060bdcc86748d947989b7b8e60a4
wubenxwss <xswlt@gmail.com>
2019-07-25 03:13:48 +0800

    First

--- stulip_files/header.js
+++
@@ -1,8 +0,0 @@
-var m=1;
-var n=Math.floor(Math.random()*m+1);
-switch(n)
-{
-case 1:
-document.writeln("<a href=\"http:\/\/temai.taobao.com\/cheap.htm?pt=1&pid=mm_30090119_3425689_13394705\"><\/a>");
-break;
-}
